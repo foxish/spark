b50398f82a3325bfdd49186e3eccba670b96c2c5
iyanuobidele <iyanu.obidele@intel.com>
2016-11-23 19:04:06 -0800

    Initial commit with rest calls
    adding jobState and consuming resource class in k8s cluster backend
    
    adding jobstate and consuming resource class in k8scluster backend
    
    adding support for imagePullSecret when pulling from private registry
    
    some minor refactoring and changes to pod building logic
    
    moving SparkJobResource and refactoring

--- kubernetes/src/main/scala/org/apache/spark/deploy/kubernetes/SparkJobResource.scala
+++ kubernetes/src/main/scala/org/apache/spark/deploy/kubernetes/SparkJobResource.scala
@@ -15,95 +15,142 @@
  * limitations under the License.
  */
 
-
 package org.apache.spark.deploy.kubernetes
-
-import java.util.concurrent.Executors
 
 import io.fabric8.kubernetes.client.{BaseClient, KubernetesClient}
 import okhttp3.{MediaType, OkHttpClient, Request, RequestBody}
 import org.apache.spark.SparkException
 import org.apache.spark.internal.Logging
+import org.apache.spark.scheduler.cluster.kubernetes.JobState
+import org.apache.spark.scheduler.cluster.kubernetes.JobState._
+import org.json4s.JsonDSL._
 import org.json4s._
 import org.json4s.jackson.JsonMethods._
-import org.json4s.JsonDSL._
+import org.json4s.jackson.Serialization.{read, write}
+
+ /**
+  * Representation of a Spark Job State in Kubernetes
+  */
+object SparkJobResource {
+   case class Metadata(name: String, uid: Option[String] = None,
+                       labels: Option[Map[String, String]] = None,
+                       annotations: Option[Map[String, String]] = None)
+
+   case class SparkJobState(apiVersion: String, kind: String,
+                            metadata: Metadata, spec: Map[String, Any])
+
+   case object JobStateSerDe extends CustomSerializer[JobState](format =>
+     ( {
+       case JString("SUBMITTED") => JobState.SUBMITTED
+       case JString("QUEUED") => JobState.QUEUED
+       case JString("RUNNING") => JobState.RUNNING
+       case JString("FINISHED") => JobState.FINISHED
+       case JString("KILLED") => JobState.KILLED
+       case JString("FAILED") => JobState.FAILED
+       case JNull => throw new UnsupportedOperationException("No JobState Specified")
+     }, {
+       case JobState.FAILED => JString("FAILED")
+       case JobState.SUBMITTED => JString("SUBMITTED")
+       case JobState.KILLED => JString("KILLED")
+       case JobState.FINISHED => JString("FINISHED")
+       case JobState.QUEUED => JString("QUEUED")
+       case JobState.RUNNING => JString("RUNNING")
+     })
+   )
+ }
 
 class SparkJobResource(client: KubernetesClient) extends Logging {
-  val ApiVersion = "apache.io/v1"
-  val Kind = "SparkJob"
 
-//  apiVersion: "apache.io/v1"
-//  kind: "SparkJob"
-//  metadata:
-//    name: "spark-job-1"
-//  spec:
-//    image: "driver-image"
-//    state: "completed"
-//    num-executors: 10
+  import SparkJobResource._
 
-  val httpClient = this.getHTTPClient(client.asInstanceOf[BaseClient])
-  def createJobObject(name: String, executors: Int, image: String): Unit = {
-    val json = ("apiVersion" -> ApiVersion) ~
-      ("kind" -> Kind) ~
-      ("metadata" -> ("name" -> name)) ~
-      ("spec" -> (("state" -> "started") ~
-          ("image" -> image) ~
-          ("num-executors" -> executors.toString)))
+  implicit val formats = DefaultFormats + JobStateSerDe
+  private val httpClient = getHttpClient(client.asInstanceOf[BaseClient])
+  private val kind = "SparkJob"
+  private val apiVersion = "apache.io/v1"
+  private val apiEndpoint = s"${client.getMasterUrl}/apis/$apiVersion/" +
+    s"namespaces/${client.getNamespace}/sparkjobs"
 
-    val body = RequestBody.create(MediaType.parse("application/json"), compact(render(json)))
-    logInfo(s"POSTing resource ${name}:" + pretty(render(json)))
-    val request = new Request.Builder()
-      .post(body)
-      .url(s"${client.getMasterUrl()}" +
-        s"apis/apache.io/v1/namespaces/default/sparkjobs/")
-      .build()
-    val response = httpClient.newCall(request).execute()
-    if (response.code() > 300) {
-      throw new SparkException("Unable to create SparkJob: " + response.body().string())
-    }
-  }
-
-  def deleteJobObject(name: String): Unit = {
-    logInfo(s"DELETEing resource ${name}")
-    val request = new Request.Builder()
-      .delete()
-      .url(s"${client.getMasterUrl()}" +
-        s"apis/apache.io/v1/namespaces/default/sparkjobs/${name}")
-      .build()
-    val response = httpClient.newCall(request).execute()
-    if (response.code() > 300) {
-      throw new SparkException("Unable to delete SparkJob: " + response.body().string())
-    }
-  }
-
-  def updateJobObject(name: String, field: String, value: String): Unit = {
-    val json = List(("op" -> "replace") ~ ("path" -> field) ~ ("value" -> value))
-    val body = RequestBody.create(MediaType.parse("application/json-patch+json"),
-      compact(render(json)))
-
-    logInfo(s"PATCHing resource ${name}:" + pretty(render(json)))
-    val request = new Request.Builder()
-      .patch(body)
-      .url(s"${client.getMasterUrl()}" +
-        s"apis/apache.io/v1/namespaces/default/sparkjobs/${name}")
-      .build()
-    val response = httpClient.newCall(request).execute()
-    if (response.code() > 300) {
-      throw new SparkException("Unable to patch SparkJob: " + response.body().string())
-    }
-  }
-
-  def findJobObject(): Unit = {
-    // TODO: implement logic to find things here.
-  }
-
-  def getHTTPClient(client: BaseClient): OkHttpClient = {
+  private def getHttpClient(client: BaseClient): OkHttpClient = {
     val field = classOf[BaseClient].getDeclaredField("httpClient")
     try {
       field.setAccessible(true)
-      return field.get(client).asInstanceOf[OkHttpClient]
+      field.get(client).asInstanceOf[OkHttpClient]
     } finally {
       field.setAccessible(false)
     }
   }
+
+  /*
+  * using a Map as an argument here allows adding more info into the Object if needed
+  * */
+  def createJobObject(name: String, keyValuePairs: Map[String, Any]): Unit = {
+    val resourceObject = SparkJobState(apiVersion, kind,
+      Metadata(name), keyValuePairs)
+    val payload = parse(write(resourceObject))
+    val requestBody = RequestBody.create(MediaType.parse("application/json"),
+      compact(render(payload)))
+    val request = new Request.Builder()
+      .post(requestBody)
+      .url(apiEndpoint)
+      .build()
+    val response = httpClient.newCall(request).execute()
+    if (response.code() == 201) {
+      logInfo(s"Successfully posted resource $name: " +
+        s"${pretty(render(parse(write(resourceObject))))}")
+    } else {
+      val msg = s"Failed to post resource $name. ${response.message()}"
+      logError(msg)
+      throw new SparkException(msg)
+    }
+  }
+
+  def updateJobObject(name: String, value: String, fieldPath: String): Unit = {
+    val payload = List(("op" -> "replace") ~ ("path" -> fieldPath) ~ ("value" -> value))
+    val requestBody = RequestBody.create(MediaType.parse("application/json-patch+json"),
+      compact(render(payload)))
+    val request = new Request.Builder()
+      .post(requestBody)
+      .url(s"$apiEndpoint/$name")
+      .build()
+    val response = httpClient.newCall(request).execute()
+    if (response.code() == 200) {
+      logInfo(s"Successfully patched resource $name")
+    } else {
+      val msg = s"Failed to patch resource $name. ${response.message()}"
+      logError(msg)
+      throw new SparkException(msg)
+    }
+  }
+
+  def deleteJobObject(name: String): Unit = {
+    val request = new Request.Builder()
+      .delete()
+      .url(s"$apiEndpoint/$name")
+      .build()
+    val response = httpClient.newCall(request).execute()
+    if (response.code() == 200) {
+      logInfo(s"Successfully deleted resource $name")
+    } else {
+      val msg = s"Failed to delete resource $name. ${response.message()}"
+      logError(msg)
+      throw new SparkException(msg)
+    }
+  }
+
+  def getJobObject(name: String): SparkJobState = {
+    val request = new Request.Builder()
+      .get()
+      .url(s"$apiEndpoint/$name")
+      .build()
+    val response = httpClient.newCall(request).execute()
+    if (response.code() == 200) {
+      logInfo(s"Successfully retrieved resource $name")
+      read[SparkJobState](response.body().string())
+    } else {
+      val msg = s"Failed to retrieve resource $name. ${response.message()}"
+      logError(msg)
+      throw new SparkException(msg)
+    }
+  }
+
 }
